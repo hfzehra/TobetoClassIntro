3db05fe1d5d99ded47469a738f50548dc8f581aa
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2023-11-09 17:38:46 +0300

    Introduction to OOP, value and reference types were processed.

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    //Ürünle ilgili operasyonlar yapmak istiyorsak sonuna 'manager' veya 'service' ekleriz 
+    internal class ProductManager
+    {
+        public void Add(Product product)
+        {
+
+        }
+    }
+}
